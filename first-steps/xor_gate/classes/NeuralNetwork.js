eaaeeb5d19f54af95d31913499d647e5047cc3ae
Linus <linussee@web.de>
2018-08-07 22:34:17 +0200

    feat: feedForward method
    Added a feedForward method to the neural network. For this method to work,
    a sigmoid activation function and a static method to multiply a matrix with a
    vector have also been added

--- first-steps/xor_gate/classes/NeuralNetwork.js
+++ first-steps/xor_gate/classes/NeuralNetwork.js
@@ -4,4 +4,30 @@ class NeuralNetwork {
 		this.outputWeights = new Array(outputCount).fill().map(e => new Array(hiddenCount).fill().map(v => Math.random() * 2 - 1));
 		this.learningRate = learningRate;
 	}
+
+	feedForward(inputVector) {
+		const hiddenOutput = NeuralNetwork.multiplyMatrixWithVector(this.hiddenWeights, inputVector);
+		const finalOutput = NeuralNetwork.multiplyMatrixWithVector(this.outputWeights, hiddenOutput);
+
+		return this.activation(finalOutput);
+	}
+
+
+	activation(inputVector) {
+		return Array.from(inputVector).map(value => this.sigmoid(value));
+	}
+
+	sigmoid(value) {
+		return (1 / (1 + Math.pow(Math.E, - value)));
+	}
+
+	static multiplyMatrixWithVector(matrix, vector) {
+		const result = new Array(matrix.length).fill(0);	// The result of a matrix - vector product must be a vector
+		for(let i = 0; i < matrix.length; i++) {
+			for(let j = 0; j < matrix[0].length; j++) {
+				result[i] = result[i] + matrix[i][j] * vector[j];
+			}
+		}
+		return result;
+	}
 }
