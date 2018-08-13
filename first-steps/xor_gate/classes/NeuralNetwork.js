6a7be393235bdc12657367bc0e12d3255a40263b
Linus <linussee@web.de>
2018-08-13 22:44:03 +0200

    feat: bias and training
    Added a simple (meaning it will not yet be trained as well) bias to the network
    and implemented the training method
    The training method is currently hardcoded for a 2-2-1 network, since I'm still
    trying to figure out backtracking

--- first-steps/xor_gate/classes/NeuralNetwork.js
+++ first-steps/xor_gate/classes/NeuralNetwork.js
@@ -2,18 +2,52 @@ class NeuralNetwork {
 	constructor(inputCount, hiddenCount, outputCount, learningRate) {
 		this.hiddenWeights = new Array(hiddenCount).fill().map(e => new Array(inputCount).fill().map(v => Math.random() * 2 - 1));
 		this.outputWeights = new Array(outputCount).fill().map(e => new Array(hiddenCount).fill().map(v => Math.random() * 2 - 1));
+		this.hiddenBias = Math.random() * 2 - 1;
+		this.outputBias = Math.random() * 2 - 1;
 		this.learningRate = learningRate;
 	}
 
 	feedForward(inputVector) {
-		const hiddenOutput = NeuralNetwork.multiplyMatrixWithVector(this.hiddenWeights, inputVector);
-		const finalOutput = NeuralNetwork.multiplyMatrixWithVector(this.outputWeights, hiddenOutput);
+		const hiddenNetInput = NeuralNetwork.multiplyMatrixWithVector(this.hiddenWeights, inputVector).map(val => val + this.hiddenBias);
+		const hiddenOutput = this.activation(hiddenNetInput);
+		const finalNetInput = NeuralNetwork.multiplyMatrixWithVector(this.outputWeights, hiddenOutput).map(val => val + this.outputBias);
 
-		return this.activation(finalOutput);
+		return this.activation(finalNetInput);
 	}
 
 	train(inputVector, expectedOutput) {
-		const output = feedForward(inputVector);
+		const hiddenNetInput = NeuralNetwork.multiplyMatrixWithVector(this.hiddenWeights, inputVector).map(val => val + this.hiddenBias);
+		const hiddenOutput = this.activation(hiddenNetInput);
+		const finalNetInput = NeuralNetwork.multiplyMatrixWithVector(this.outputWeights, hiddenOutput).map(val => val + this.outputBias);
+		const finalOutput = this.activation(finalNetInput);
+
+		// From here on out its only made for a 2-2-1 network (Only temporary)
+		const errorGradient = finalOutput[0] - expectedOutput[0];
+		const outputGradient = finalNetInput.map(val => this.sigmoidDerivative(val))[0];
+		console.log("ErrGrad:", errorGradient);
+		console.log("OutGrad:", outputGradient);
+
+		const weight5Gradient = errorGradient * outputGradient * hiddenOutput[0];
+		const weight6Gradient = errorGradient * outputGradient * hiddenOutput[1];
+		console.log("W5Grad:", weight5Gradient);
+		console.log("W6Grad:", weight6Gradient);
+
+		const weight1Gradient = errorGradient * outputGradient * this.outputWeights[0][0] * hiddenNetInput.map(val => this.sigmoidDerivative(val))[0] * inputVector[0];
+		const weight2Gradient = errorGradient * outputGradient * this.outputWeights[0][0] * hiddenNetInput.map(val => this.sigmoidDerivative(val))[0] * inputVector[1];
+		const weight3Gradient = errorGradient * outputGradient * this.outputWeights[0][1] * hiddenNetInput.map(val => this.sigmoidDerivative(val))[1] * inputVector[0];
+		const weight4Gradient = errorGradient * outputGradient * this.outputWeights[0][1] * hiddenNetInput.map(val => this.sigmoidDerivative(val))[1] * inputVector[1];
+		console.log("W1Grad", weight1Gradient);
+		console.log("W2Grad", weight2Gradient);
+		console.log("W3Grad", weight3Gradient);
+		console.log("W4Grad", weight4Gradient);
+
+		this.hiddenWeights[0][0] -= this.learningRate * weight1Gradient;
+		this.hiddenWeights[0][1] -=	this.learningRate * weight2Gradient;
+		this.hiddenWeights[1][0] -= this.learningRate * weight3Gradient;
+		this.hiddenWeights[1][1] -= this.learningRate * weight4Gradient;
+
+		this.outputWeights[0][0] -= this.learningRate * weight5Gradient;
+		this.outputWeights[0][1] -= this.learningRate * weight6Gradient;
 	}
 
 	activation(inputVector) {
@@ -26,7 +60,7 @@ class NeuralNetwork {
 
 	sigmoidDerivative(value) {
 		const sigmoidVal = this.sigmoid(value);
-		return (sigmoidVal * (1 - sigmoidVal))
+		return sigmoidVal * (1 - sigmoidVal);
 	}
 
 	meanSquaredError(actualOutput, expectedOutput) {
