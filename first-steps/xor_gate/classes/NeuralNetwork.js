f04663d6e90d29dfdb30cfc67b8b87f85e6196c6
Linus <linussee@web.de>
2018-08-14 17:15:01 +0200

    refactor: commented and removed unneeded logs and comments
    Added some comments, mostly to explain a method in a short sentence and
    removed all logs and superfluous comments

--- first-steps/xor_gate/classes/NeuralNetwork.js
+++ first-steps/xor_gate/classes/NeuralNetwork.js
@@ -2,11 +2,10 @@ class NeuralNetwork {
 	constructor(inputCount, hiddenCount, outputCount, learningRate) {
 		this.hiddenWeights = new Array(hiddenCount).fill().map(e => new Array(inputCount + 1).fill().map(v => Math.random() * 2 - 1));
 		this.outputWeights = new Array(outputCount).fill().map(e => new Array(hiddenCount + 1).fill().map(v => Math.random() * 2 - 1));
-		//this.hiddenBias = Math.random() * 2 - 1;
-		//this.outputBias = Math.random() * 2 - 1;
 		this.learningRate = learningRate;
 	}
 
+	// Returns the current result/guess of the network for the given input vector
 	feedForward(inputVector) {
 		const hiddenNetInput = NeuralNetwork.multiplyMatrixWithVector(this.hiddenWeights, this.biasedVector(inputVector));
 		const hiddenOutput = this.activation(hiddenNetInput);
@@ -15,16 +14,19 @@ class NeuralNetwork {
 		return this.activation(finalNetInput);
 	}
 
+	// Takes a vector and add a bias of 1.0 to its end
 	biasedVector(inputVector) {
 		const newVector = Array.from(inputVector);
 		newVector.push(1.0);
 		return newVector;
 	}
 
+	// Takes a single data entry to train the network according to its expected output
 	train(inputVector, expectedOutput) {
 		this.trainBatch([inputVector], [expectedOutput]);
 	}
 
+	// Takes and entire batch (list) of inputs and trains the network with them according to their expected outputs
 	trainBatch(inputs, expectedOutputs) {
 		let weight1Gradient = 0;
 		let weight2Gradient = 0;
@@ -37,12 +39,13 @@ class NeuralNetwork {
 		let bias3Gradient = 0;
 
 		for(let i = 0; i < inputs.length; i++) {
+			// Calculate netInput and output for each layer
 			const hiddenNetInput = NeuralNetwork.multiplyMatrixWithVector(this.hiddenWeights, this.biasedVector(inputs[i]));
 			const hiddenOutput = this.activation(hiddenNetInput);
 			const finalNetInput = NeuralNetwork.multiplyMatrixWithVector(this.outputWeights, this.biasedVector(hiddenOutput));
 			const finalOutput = this.activation(finalNetInput);
 
-			// From here on out its only made for a 2-2-1 network (Only temporary)
+			// From here on out its only made for a 2-2-1 network
 			const errorGradient = finalOutput[0] - expectedOutputs[i][0];
 			const outputGradient = finalNetInput.map(val => this.sigmoidDerivative(val))[0];
 
@@ -59,6 +62,7 @@ class NeuralNetwork {
 			bias2Gradient += errorGradient * outputGradient * this.outputWeights[0][1] * derivedHiddenNetInput[1];
 			bias3Gradient += errorGradient * outputGradient;
 		}
+		// Adjust all weights and biases
 		this.hiddenWeights[0][0] -= this.learningRate * weight1Gradient;
 		this.hiddenWeights[0][1] -=	this.learningRate * weight2Gradient;
 		this.hiddenWeights[1][0] -= this.learningRate * weight3Gradient;
@@ -72,19 +76,23 @@ class NeuralNetwork {
 		this.outputWeights[0][2] -= this.learningRate * bias3Gradient;
 	}
 
+	// Takes an inputVector and applies the activation function (in this case sigmoid) to each entry
 	activation(inputVector) {
 		return Array.from(inputVector).map(value => this.sigmoid(value));
 	}
 
+	// Applies the sigmoid function to a single value
 	sigmoid(value) {
 		return (1 / (1 + Math.pow(Math.E, - value)));
 	}
 
+	// Applies the sigmoid function's derivative to a single value
 	sigmoidDerivative(value) {
 		const sigmoidVal = this.sigmoid(value);
 		return sigmoidVal * (1 - sigmoidVal);
 	}
 
+	// Calculates the mean squared error of the network. Not really needed in this project
 	meanSquaredError(actualOutput, expectedOutput) {
 		let result = 0;
 		for(let i = 0; i < actualOutput.length; i++) {
@@ -94,6 +102,7 @@ class NeuralNetwork {
 		return result;
 	}
 
+	// Multiplies the given matrix with the given vector
 	static multiplyMatrixWithVector(matrix, vector) {
 		const result = new Array(matrix.length).fill(0);	// The result of a matrix - vector product must be a vector
 		for(let i = 0; i < matrix.length; i++) {
@@ -101,9 +110,6 @@ class NeuralNetwork {
 				result[i] = result[i] + matrix[i][j] * vector[j];
 			}
 		}
-		// console.table(matrix);
-		// console.table(vector);
-		// console.table(result);
 		return result;
 	}
 }
--- first-steps/xor_gate/main.js
+++ first-steps/xor_gate/main.js
@@ -1,12 +1,14 @@
 class Main {
 	constructor() {
 		this.nn = new NeuralNetwork(2, 2, 1, 0.5);
+		// Dataset (in this case all possible inputs)
 		this.xorInputs = [
 			[ 1, 1],
 			[ 1, 0],
 			[ 0, 1],
 			[ 0, 0]
 		];
+		// Solution to the dataset (in this case all possible outputs)
 		this.xorOutputs = [
 			[0],
 			[1],
@@ -15,6 +17,7 @@ class Main {
 		];
 	}
 
+	// Evaluates the current solution of the network for each possible input and displays them in the webpage
 	evaluate() {
 		document.getElementById("outputOne").innerHTML = this.nn.feedForward([this.xorInputs[0][0], this.xorInputs[0][1]]);
 		document.getElementById("outputTwo").innerHTML = this.nn.feedForward([this.xorInputs[1][0], this.xorInputs[1][1]]);
@@ -22,20 +25,12 @@ class Main {
 		document.getElementById("outputFour").innerHTML = this.nn.feedForward([this.xorInputs[3][0], this.xorInputs[3][1]]);
 	}
 
+	// Trains the neural network with all input and output for iterationCount times
 	train(iterationCount) {
 		for(let i = 0; i < iterationCount; i++) {
 			this.nn.trainBatch(this.xorInputs, this.xorOutputs);
 		}
 		this.evaluate();
-		/*// TODO: Somehow train all training data at once
-		for(let i = 0; i < iterationCount; i++) {
-			for(let j = 0; j < this.xorValues.length; j++) {
-				// This is not a correct usage of gradient descent (needs a fix)
-				const val = this.xorValues[j];
-				this.nn.train([val[0], val[1]], this.xorOutputs[j]);
-			}
-		}
-		this.evaluate();*/
 	}
 }
 
