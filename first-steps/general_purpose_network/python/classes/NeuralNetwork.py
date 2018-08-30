ba2b9eefaf634d93fa298d1d775fc3783537f12e
Linus <linussee@web.de>
2018-08-30 23:36:53 +0200

    feat: basic network skeleton

---
+++ first-steps/general_purpose_network/python/classes/NeuralNetwork.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class NeuralNetwork:
+	def __init__(self, neuronsPerLayer, learningRate):
+		self.neuronsPerLayer = neuronsPerLayer
+		#self.weights = [ np.random.rand() for ]
+
+	def feedforward():
+
+
+	def trainBatch():
+
