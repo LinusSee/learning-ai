f876592f9ee66efdfe14ee0c49ddf2aba288e277
Linus <linussee@web.de>
2018-08-31 11:50:18 +0200

    feat: entry point file and network logic
    Added a main.py file that will be the entrance point for invoking network
    methods.
    Made NeuralNetork.py importable by __init__.py file
    NeuralNetwork now creates weights and does the feedforward correctly

--- first-steps/general_purpose_network/python/classes/NeuralNetwork.py
+++ first-steps/general_purpose_network/python/classes/NeuralNetwork.py
@@ -1,12 +1,31 @@
 import numpy as np
 
 class NeuralNetwork:
-	def __init__(self, neuronsPerLayer, learningRate):
+	def __init__(self, neuronsPerLayer, learningRate=0.5):
 		self.neuronsPerLayer = neuronsPerLayer
-		#self.weights = [ np.random.rand() for ]
+		# First zip argument excludes last element and second argument excludes first element
+		self.weights = [np.random.rand(y, x + 1) for x, y in zip(neuronsPerLayer[:-1], neuronsPerLayer[1:])]
+		print(self.weights)
 
-	def feedforward():
+	def feedforward(self, input):
+		currentOutput = np.array(input)
+		for weightMatrix in self.weights:
+			temp = self.biasedVector(currentOutput)
+			temp = np.dot(weightMatrix, temp)
+			currentOutput = self.sigmoid(temp)
+
+		return currentOutput
 
 
-	def trainBatch():
+	def trainBatch(self):
+		print("Currently empty method")
+
+	def sigmoid(self, value):
+		# Addition and division is automatically done elementwise for
+		# numpy arrays
+		return 1 / (1 + np.exp(-value))
+
+	# Expects a numpy array
+	def biasedVector(self, vector):
+		return np.append(vector, 1)
 
---
+++ first-steps/general_purpose_network/python/main.py
@@ -0,0 +1,4 @@
+from classes.NeuralNetwork import NeuralNetwork
+
+nn = NeuralNetwork([2, 2, 1])
+nn.feedforward([1, 0])
