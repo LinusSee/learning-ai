3641dcb962d326945a43d206b9edef2d4b6b447e
Linus <linussee@web.de>
2018-08-31 23:24:45 +0200

    feat: training algorithm
    Training algorithm is not finished. Needs a few more test examples.

--- first-steps/general_purpose_network/python/classes/NeuralNetwork.py
+++ first-steps/general_purpose_network/python/classes/NeuralNetwork.py
@@ -5,7 +5,7 @@ class NeuralNetwork:
 		self.neuronsPerLayer = neuronsPerLayer
 		# First zip argument excludes last element and second argument excludes first element
 		self.weights = [np.random.rand(y, x + 1) for x, y in zip(neuronsPerLayer[:-1], neuronsPerLayer[1:])]
-		print(self.weights)
+		self.learningRate = learningRate
 
 	def feedforward(self, input):
 		currentOutput = np.array(input)
@@ -17,15 +17,44 @@ class NeuralNetwork:
 		return currentOutput
 
 
-	def trainBatch(self):
-		print("Currently empty method")
+	def trainBatch(self, inputs, outputs):
+		gradients = []
+		for input, expectedOutput in zip(inputs, outputs):
+			netValues = []
+			outValues = [input]
+			for weightMatrix in self.weights:
+				temp = self.biasedVector(outValues[-1]) # Last element = latestOutput
+				netValues.append(np.dot(weightMatrix, temp))
+				outValues.append(self.sigmoid(netValues[-1]))
+
+			currentDerivative = self.sigmoid_prime(outValues[-1], useSigmoid=False)
+			for x in range(len(currentDerivative)):
+				currentDerivative[x] *= (outValues[-1][x] - expectedOutput[x])
+			gradients.append(np.dot(np.transpose([currentDerivative]), [self.biasedVector(outValues[-2])]))
+			for x in reversed(range(1, len(self.weights))):
+				temp = np.dot(np.transpose(self.weightsWithoutBias(self.weights[x])), currentDerivative)
+				currentDerivative = np.multiply(temp, self.sigmoid_prime(outValues[x], useSigmoid=False))
+				gradients.append(np.dot(np.transpose([currentDerivative]), [self.biasedVector(outValues[x - 1])]))
+
+		for index, gradient in enumerate(reversed(gradients)):
+			self.weights[index % len(self.weights)] -= gradient * self.learningRate
+
+
 
 	def sigmoid(self, value):
 		# Addition and division is automatically done elementwise for
 		# numpy arrays
 		return 1 / (1 + np.exp(-value))
 
+	def sigmoid_prime(self, value, useSigmoid=True):
+		if useSigmoid:
+			value = self.sigmoid(value)
+		return value * (1 - value)
+
 	# Expects a numpy array
 	def biasedVector(self, vector):
 		return np.append(vector, 1)
+
+	def weightsWithoutBias(self, weights):
+		return np.delete(weights, weights.shape[1] - 1, 1)
 
--- first-steps/general_purpose_network/python/main.py
+++ first-steps/general_purpose_network/python/main.py
@@ -1,4 +1,24 @@
 from classes.NeuralNetwork import NeuralNetwork
 
 nn = NeuralNetwork([2, 2, 1])
-nn.feedforward([1, 0])
+#print(nn.feedforward([1, 0]))
+
+xorInputs = [
+	[1, 1],
+	[1, 0],
+	[0, 1],
+	[0, 0]
+]
+xorOutputs = [
+	[0],
+	[1],
+	[1],
+	[0]
+]
+for x in range(0, 10000):
+	nn.trainBatch(xorInputs, xorOutputs)
+
+print(nn.feedforward([1, 1]))
+print(nn.feedforward([1, 0]))
+print(nn.feedforward([0, 1]))
+print(nn.feedforward([0, 0]))
