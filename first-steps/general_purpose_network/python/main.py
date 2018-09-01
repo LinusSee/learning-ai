d190ae9850f24dc78e38544e672fcfb12b3ec86f
Linus <linussee@web.de>
2018-09-01 12:09:07 +0200

    feat: testcases (same as for the javascript network)
    Added some testcases, commented out one example though, because training
    it took so many iterations, that computation time for that example alone
    took several minutes

--- first-steps/general_purpose_network/python/main.py
+++ first-steps/general_purpose_network/python/main.py
@@ -1,9 +1,13 @@
 from classes.NeuralNetwork import NeuralNetwork
 
-nn = NeuralNetwork([2, 2, 1])
-#print(nn.feedforward([1, 0]))
+xorNet = NeuralNetwork([2, 2, 1])
+andNet = NeuralNetwork([2, 1])
+orNet = NeuralNetwork([2, 1])
+bigXorNet = NeuralNetwork([2, 2, 2, 1])
+rlyBigXorNet = NeuralNetwork([2, 6, 8, 6, 2, 1])
+switchNet = NeuralNetwork([2, 4, 3, 2])
 
-xorInputs = [
+inputs = [
 	[1, 1],
 	[1, 0],
 	[0, 1],
@@ -15,10 +19,45 @@ xorOutputs = [
 	[1],
 	[0]
 ]
-for x in range(0, 10000):
-	nn.trainBatch(xorInputs, xorOutputs)
+andOutputs = [
+	[1],
+	[0],
+	[0],
+	[0]
+]
+orOutputs = [
+	[1],
+	[1],
+	[1],
+	[0]
+]
+switchOutputs = [
+	[1, 1],
+	[0, 1],
+	[1, 0],
+	[0, 0]
+]
 
-print(nn.feedforward([1, 1]))
-print(nn.feedforward([1, 0]))
-print(nn.feedforward([0, 1]))
-print(nn.feedforward([0, 0]))
+for x in range(0, 10000):
+	xorNet.trainBatch(inputs, xorOutputs)
+	andNet.trainBatch(inputs, andOutputs)
+	orNet.trainBatch(inputs, orOutputs)
+	bigXorNet.trainBatch(inputs, xorOutputs)
+	# Commented out because training didn't even work with 100.000 iterations
+	# Tried 1million, which worked but took several minutes (about 10)
+	#rlyBigXorNet.trainBatch(inputs, xorOutputs)
+	switchNet.trainBatch(inputs, switchOutputs)
+
+def printOutput(network):
+	print(network.feedforward(inputs[0]))
+	print(network.feedforward(inputs[1]))
+	print(network.feedforward(inputs[2]))
+	print(network.feedforward(inputs[3]))
+	print("\n")
+
+printOutput(xorNet)
+printOutput(andNet)
+printOutput(orNet)
+printOutput(bigXorNet)
+printOutput(rlyBigXorNet)
+printOutput(switchNet)
