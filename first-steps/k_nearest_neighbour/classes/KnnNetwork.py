edd7e09ab8633c7a79b8e9834e3ac1d7aef7cb7a
Linus <linussee@web.de>
2018-09-06 17:22:31 +0200

    feat: init file and euclidianDistance method
    Added an init file so the network can be imported and a method
    to calculate the euclidian distance

--- first-steps/k_nearest_neighbour/classes/KnnNetwork.py
+++ first-steps/k_nearest_neighbour/classes/KnnNetwork.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 class KnnNetwork:
+
+	def __init__(self):
+		print("Constructor")
+
+	# Works with numpy vectors
+	def euclidianDistance(self, data1, data2):
+		differenceSquared = np.square(data1 - data2)
+		sumOfSquares = np.sum(differenceSquared)
+
+		return np.sqrt(sumOfSquares)
 
